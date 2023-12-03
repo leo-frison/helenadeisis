1c8ece992d60083c1ca8f2028eeea1eac7c50326
Leonardo Frison <leonart_crady205@hotmail.com>
2023-12-03 19:36:35 -0300

    Updates

---
+++ precache-manifest.148c64baef72eba7047f4d5f8fd427fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8eaf6fa9a618e7ec7b902f8f4fbc46d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "170122ebd5da864c2605",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "b509ca54b7589e3628fa",
+    "url": "/static/css/main.5cfe863e.chunk.css"
+  },
+  {
+    "revision": "170122ebd5da864c2605",
+    "url": "/static/js/2.a93e5d36.chunk.js"
+  },
+  {
+    "revision": "b509ca54b7589e3628fa",
+    "url": "/static/js/main.4d63df8c.chunk.js"
+  },
+  {
+    "revision": "bf6a2740ebbe407bc679",
+    "url": "/static/js/runtime-main.a9910a8c.js"
+  },
+  {
+    "revision": "844d83d6c252e790e1a007b784295188",
+    "url": "/static/media/hell.844d83d6.png"
+  },
+  {
+    "revision": "1186d41f728e0ccb73115a5a2fa0dd0b",
+    "url": "/static/media/logo.1186d41f.png"
+  }
+]);
